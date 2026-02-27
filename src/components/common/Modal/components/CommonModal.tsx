532b8ef47bce09a77ec29d7e076bffa1753c6516
saurav.sapariya <saurav.sapariya@agileinfoways.com>
2026-02-27 23:34:06 +0530

    worked on modal design changes

--- src/components/common/Modal/components/CommonModal.tsx
+++ src/components/common/Modal/components/CommonModal.tsx
@@ -1,13 +1,17 @@
 import { ModalWrapper } from '../Modal.Styled';
 import { IModalProps } from '../types';
 
+const DEFAULT_MODAL_Z_INDEX = 2000;
+
 const CommonModal = (props: IModalProps) => {
   const {
     className = '',
     footer,
     closable = true,
     maskClosable = true,
-    destroyOnClose = true
+    destroyOnClose = true,
+    zIndex,
+    getContainer
   } = props;
   return (
     <ModalWrapper
@@ -16,6 +20,10 @@ const CommonModal = (props: IModalProps) => {
       footer={footer}
       maskClosable={maskClosable}
       destroyOnClose={destroyOnClose}
+      // Ensure modals always appear above the app header (header z-index is 1001).
+      zIndex={zIndex ?? DEFAULT_MODAL_Z_INDEX}
+      // Ensure modal is not clipped by any scroll/overflow containers.
+      getContainer={getContainer ?? (() => document.body)}
       className={`common-modal ${className}`}
     >
       {props.children}
